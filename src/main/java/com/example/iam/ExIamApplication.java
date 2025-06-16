d1f6547398ed67ba275c31fe8b28bd77968d0313
phongpq <v.phongpq@vinhomes.vn>
2025-06-16 22:09:14 +0700

    init commit

---
+++ src/main/java/com/example/iam/ExIamApplication.java
@@ -0,0 +1,13 @@
+package com.example.iam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExIamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExIamApplication.class, args);
+	}
+
+}
